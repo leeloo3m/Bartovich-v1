47d9d0d348a9503e676affc8eeff2d26c10fac2c
Londort <landort@gmail.com>
2021-11-10 09:20:30 +0100

    cards animation at review.html

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
 		$(".headermobile__burger,.headermobile__nav").toggleClass("active");
 		$("body").toggleClass("lock");
 	});
-});
+
+	$('.review__wrap').each(function () {
+		if ($(this).hover(function () {
+			$(this).find('.review__footer').toggleClass('brd'); //найти элемент внутри выбранного this
+		}));
+	})
+})
